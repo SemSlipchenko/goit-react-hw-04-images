910208cba7631c4a2b52e3b608570861773d5bec
SemSlipchenko <semslipchenko@gmail.com>
2022-11-06 16:37:45 +0200

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import css from '../Button/Button.module.css';
 const Button = ({ onClick }) => {
   return (
     <button className={css.Button} type="button" onClick={onClick}>
-      Load moe
+      Load more
     </button>
   );
 };
